b5d0848f566e5d2ec8756b654b27559e0c8a5068
Ajay Shrestha <er.ajaystha@gmail.com>
2023-05-15 13:36:38 -0400

    playlist app setup

---
+++ apps/playlist/src/App.tsx
@@ -0,0 +1,29 @@
+import "./App.css";
+import { AppShell } from "ui";
+import MoviesContent from "./MoviesContent";
+
+function App() {
+  return (
+    <div>
+      <AppShell
+        navLinks={[
+          {
+            label: "Home",
+            path: "/",
+          },
+          {
+            label: "Playlist",
+            path: "/playlist",
+          },
+        ]}
+        routes={[
+          { path: "/", element: MoviesContent },
+          { path: "/playlist", element: () => <div> Playlist</div> },
+        ]}
+        title="Movies"
+      />
+    </div>
+  );
+}
+
+export default App;
